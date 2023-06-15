9a8d6e5e430b9558a878d302f4174e77ead8a592
Or Kopelman <61802394+orkopelman@users.noreply.github.com>
2023-06-15 12:04:18 +0300

    Add files via upload

---
+++ tests/utest.cpp
@@ -0,0 +1,12 @@
+#include "level.hpp"
+
+using namespace bs;
+
+
+
+int main() {
+    
+    Level l1;
+    l1.RunLevel(); 
+
+} // bs
---
+++ tests/utest.d
@@ -0,0 +1,19 @@
+utest.o: utest.cpp ../inc/level.hpp ../inc/ball.hpp ../inc/circle.hpp \
+ ../inc/inl/circle.hxx ../inc/paddle.hpp ../inc/rectangle.hpp \
+ ../inc/inl/../inl/rectangle.hxx ../inc/brick.hpp
+
+../inc/level.hpp:
+
+../inc/ball.hpp:
+
+../inc/circle.hpp:
+
+../inc/inl/circle.hxx:
+
+../inc/paddle.hpp:
+
+../inc/rectangle.hpp:
+
+../inc/inl/../inl/rectangle.hxx:
+
+../inc/brick.hpp:
